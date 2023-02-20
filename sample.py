ce05757e27527692fcfc39a83e42b2f9e1de2af4
Manohar K <125081485+Manoharkallem@users.noreply.github.com>
2023-02-20 11:55:22 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,6 +7,6 @@ print ("THIS IS SAMPLE PYTHON PROGRAM")
 print ("\n")
 
 print ("THIS IS JENKINS Integration")
-       print ("THIS IS this is hamdsome kmanohar")
-       print ("THIS IS this is aravind")
+print ("THIS IS this is hamdsome kmanohar")
+print ("THIS IS this is aravind")
        
