1b08106f4b67e326b2efb70be2f22bbcf72220fa
Manohar K <125081485+Manoharkallem@users.noreply.github.com>
2023-02-20 12:02:54 +0530

    Update sample.py
    name added

--- sample.py
+++ sample.py
@@ -10,4 +10,5 @@ print ("THIS IS JENKINS Integration")
 print ("THIS IS this is hamdsome kmanohar")
 print ("THIS IS this is aravind")
 print ("THIS IS this is kraju")
+print ("THIS IS this is ")
        
